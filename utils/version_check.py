1372998eca8bead2801b4d09482552c2df93a78e
Anthony Benjamin <anthony@vast.ai>
2025-03-26 18:43:25 -0700

    chore: rm dead code

---
+++ utils/version_check.py
@@ -0,0 +1,10 @@
+# def get_distribution():
+#
+def is_pip_package():
+    return NotImplemented
+
+def check_local_version():
+    return NotImplemented
+
+def check_stable_version():
+    return NotImplemented

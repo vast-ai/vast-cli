6dde77ea3e5f6d2ea05f2892c5409a2bd11fec3b
Anthony Benjamin <anthony@vast.ai>
2025-03-28 17:17:33 -0700

    feat(wip): version checking

--- utils/version_checker.py
+++ utils/version_checker.py
@@ -1,28 +1,9 @@
 import os
-import importlib.metadata
-
-PACKAGE_NAME = "vast-cli-fork"
 
 
 def is_git_distribution() -> bool:
-    try:
-        # Get package location
-        dist = importlib.metadata.distribution(PACKAGE_NAME)
-        package_location = dist.locate_file("")
-
-        package_path = str(package_location)
-
-        marker_path = os.path.join(package_path, "DISTRIBUTION_SOURCE")
-        if os.path.exists(marker_path):
-            with open(marker_path, "r") as f:
-                source = f.read().strip()
-            return source != "pypi"
-        else:
-            # No marker file = git installation
-            return True
-    except Exception:
-        # Any error = assume git installation
-        return True
+    # Check if .git directory exists at the same level as the current working directory
+    return os.path.isdir(os.path.join(os.getcwd(), ".git"))
 
 
 print(is_git_distribution())

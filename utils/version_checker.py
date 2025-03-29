99f3cce7858e9936641bdfc4aa70cc02d7efa11a
Anthony Benjamin <anthony@vast.ai>
2025-03-28 18:25:05 -0700

    feat(wip): version checking

--- utils/version_checker.py
+++ utils/version_checker.py
@@ -1,9 +1,26 @@
-import os
+import importlib.metadata
 
 
 def is_git_distribution() -> bool:
-    # INFO - Check if .git directory exists at the same level as the current working directory
-    return os.path.isdir(os.path.join(os.getcwd(), ".git"))
+    """
+    Determine how the code is being run:
+    - From PyPI installation
+    - From source code (Git or direct script)
+    """
+    # Get the module/package name
+    module_name = __name__.split(".")[0]
+
+    try:
+        # Check if the package is installed with metadata
+        # This will succeed for PyPI/pip installed packages
+        metadata = importlib.metadata.metadata(module_name)
+        return False
+    except importlib.metadata.PackageNotFoundError:
+        return True
+        # if os.path.isdir(os.path.join(os.getcwd(), ".git")):
+        #     return "git_repository"
+        # else:
+        #     return "direct_script"
 
 
-print(is_git_distribution())
+print(f"Running as: {is_git_distribution()}")

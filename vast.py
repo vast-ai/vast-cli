3c94028baf0a96a8753905fffc604639efcb94aa
Anthony Benjamin <anthony@vast.ai>
2025-04-11 10:41:15 -0700

    fix: cli module import

--- vast.py
+++ vast.py
@@ -29,10 +29,11 @@ import logging
 import textwrap
 from pathlib import Path
 import warnings
+import importlib.metadata
 
-from utils.config import should_check_for_update
-from utils.version_checker import check_for_update, get_local_version, is_pip_package
-
+PYPI_BASE_PATH = "https://pypi.org"
+# INFO - Change to False if you don't want to check for update each run.
+should_check_for_update = True
 ARGS = None
 TABCOMPLETE = False
 try:
@@ -74,6 +75,92 @@ server_url_default = "https://console.vast.ai"
 logging.basicConfig(
     level=os.getenv("LOGLEVEL") or logging.WARN, format="%(levelname)s - %(message)s"
 )
+
+def parse_version(version: str) -> tuple[int, ...]:
+    parts = version.split(".")
+
+    if len(parts) < 3:
+        print(f"Invalid version format: {version}", file=sys.stderr)
+
+    return tuple(int(part) for part in parts)
+
+
+def get_git_version():
+    try:
+        result = subprocess.run(
+            ["git", "describe", "--tags", "--abbrev=0"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        tag = result.stdout.strip()
+
+        return tag[1:] if tag.startswith("v") else tag
+    except Exception:
+        return "0.0.0"
+
+
+def get_pip_version():
+    return importlib.metadata.version("vast-cli-fork")
+
+
+def is_pip_package():
+    script_path = sys.argv[0]
+    executable_name = os.path.basename(script_path)
+
+    return executable_name != "vast.py"
+
+
+def get_update_command(stable_version: str) -> str:
+    if is_pip_package():
+        if "test.pypi.org" in PYPI_BASE_PATH:
+            return f"{sys.executable} -m pip install --force-reinstall --no-cache-dir -i {PYPI_BASE_PATH} vast-cli-fork=={stable_version}"
+        else:
+            return f"{sys.executable} -m pip install --force-reinstall --no-cache-dir vast-cli-fork=={stable_version}"
+    else:
+        return f"git fetch --all --tags --prune && git checkout tags/v{stable_version}"
+
+
+def get_local_version():
+    if is_pip_package():
+        return get_pip_version()
+    return get_git_version()
+
+
+def check_for_update():
+    pypi_data = get_project_data("vast-cli-fork")
+    pypi_version = get_pypi_version(pypi_data)
+
+    local_version = get_local_version()
+
+    local_tuple = parse_version(local_version)
+    pypi_tuple = parse_version(pypi_version)
+
+    if local_tuple >= pypi_tuple:
+        return
+
+    user_wants_update = input(
+        f"Update available from {local_version} to {pypi_version}. Would you like to update [Y/n]: "
+    ).lower()
+
+    if user_wants_update not in ["y", ""]:
+        print("You selected no. If you don't want to check for updates each time, update vast_config.py")
+        return
+
+    update_command = get_update_command(pypi_version)
+
+    print("Updating...")
+    _ = subprocess.run(
+        update_command,
+        shell=True,
+        check=True,
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    print("Update completed successfully!\nAttempt to run your command again!")
+    sys.exit(0)
 
 APP_NAME = "vastai"
 VERSION = get_local_version()
@@ -223,6 +310,34 @@ def http_post(args, req_url, headers, json={}):
 def http_del(args, req_url, headers, json={}):
     return http_request("DELETE", args, req_url, headers, json)
 
+
+
+def get_project_data(project_name: str) -> dict[str, dict[str, str]]:
+    url = PYPI_BASE_PATH + f"/pypi/{project_name}/json"
+    response = requests.get(url, headers={"Accept": "application/json"})
+
+    # this will raise for HTTP status 4xx and 5xx
+    response.raise_for_status()
+
+    # this will raise for HTTP status >200,<=399
+    if response.status_code != 200:
+        raise Exception(
+            f"Could not get PyPi Project: {project_name}. Response: {response.status_code}"
+        )
+
+    response_data: dict[str, dict[str, str]] = response.json()
+    return response_data
+
+
+def get_pypi_version(project_data: dict[str, dict[str, str]]) -> str:
+    info_data = project_data.get("info")
+
+    if not info_data:
+        raise Exception("Could not get PyPi Project")
+
+    version_data: str = str(info_data.get("version"))
+
+    return str(version_data)
 
 def load_permissions_from_file(file_path):
     with open(file_path, "r") as file:

c270ae966b8f5a1820e09bc1ff0a94a729b70c1a
vast-sf-z3x0 <zuby@vast.ai>
2026-04-07 12:57:57 -0700

    readd vast.py stuff, delete requirements.txt, move shell scripts

---
+++ vast_config.py
@@ -0,0 +1,5 @@
+# DEPRECATED: This file is kept for backwards compatibility.
+# Please use the vastai package instead.
+
+# INFO: Set this to False if you don't want to check for updates when using the CLI.
+should_check_for_update=True

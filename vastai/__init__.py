07fddf3a945079d69cb29261e9c39daf7176bd64
vast-sf-z3x0 <zuby@vast.ai>
2026-04-08 12:53:52 -0700

    add missing logging

--- vastai/__init__.py
+++ vastai/__init__.py
@@ -1,3 +1,26 @@
+import logging as _logging
+import os as _os
+
+_VAST_LOG_LEVELS = {
+    "critical": _logging.CRITICAL,
+    "error": _logging.ERROR,
+    "warning": _logging.WARNING,
+    "info": _logging.INFO,
+    "debug": _logging.DEBUG,
+}
+_env_level = _os.environ.get("VAST_LOG_LEVEL", "").lower()
+_level = (
+    _VAST_LOG_LEVELS.get(_env_level, _logging.WARNING) if _env_level else _logging.INFO
+)
+
+logger = _logging.getLogger("vastai")
+logger.setLevel(_level)
+if not logger.handlers:
+    _handler = _logging.StreamHandler()
+    _handler.setFormatter(_logging.Formatter("%(levelname)s - %(message)s"))
+    logger.addHandler(_handler)
+    logger.propagate = False
+
 from .sdk import VastAI
 
 try:
